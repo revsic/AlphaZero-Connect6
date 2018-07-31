532ce12eb5d779f5f29969351d7dfd28645f4f1b
revsic <revsic99@gmail.com>
2018-08-01 05:03:37 +0900

    Update Game - Remove public asscessor

--- Connect6/src/game/mod.rs
+++ Connect6/src/game/mod.rs
@@ -1,8 +1,7 @@
 pub use self::player::Player;
-pub use self::game_impl::*;
+pub use self::game_impl::{PlayResult, Game};
 
+mod player;
 mod position;
+mod game_impl;
 mod winner_searcher;
-
-pub mod player;
-pub mod game_impl;
