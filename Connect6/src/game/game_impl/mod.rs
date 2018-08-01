d90e5d7c1e1d01daeffc03b2acd382cbd743ed9f
revsic <revsic99@gmail.com>
2018-08-02 05:27:55 +0900

    Update GameImpl - Add Parameter Writer for print

--- Connect6/src/game/game_impl/mod.rs
+++ Connect6/src/game/game_impl/mod.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests;
 
+use std::io;
 use super::player::Player;
 use super::position::Pos;
 
@@ -92,27 +93,56 @@ impl Game {
         self.num_remain
     }
 
-    pub fn print(&self) {
-        fn idx2alpha(idx: u8) -> char {
-            return (idx + 0x61) as char;
-        }
+    pub fn print<'a>(&self, writer: &'a mut io::Write) -> io::Result<usize> {
+        let mut paint = Paint::new(writer);
+        paint.push(b"0 A B C D E F G H I J K L M N O P Q R S\n");
 
-        println!("0 A B C D E F G H I J K L M N O P Q R S");
         for i in 0..19 {
-            print!("{} ", idx2alpha(i as u8));
+            let row_name = [0x61 + i as u8, ' ' as u8];
+            paint.push(&row_name);
+
             for j in 0..19 {
                 match self.board[i][j] {
-                    Player::Black => print!("X "),
-                    Player::White => print!("O "),
-                    Player::None => print!("_ "),
+                    Player::Black => paint.push(b"X "),
+                    Player::White => paint.push(b"O "),
+                    Player::None => paint.push(b"_ "),
                 }
             }
-            println!();
+            paint.push_one('\n' as u8);
         }
+        paint.write()
     }
 
     pub fn is_game_end(&self) -> Player {
         use super::winner_searcher::search;
         search(&self.board)
+    }
+}
+
+struct Paint<'a> {
+    vec: Vec<u8>,
+    writer: &'a mut io::Write,
+}
+
+impl<'a> Paint<'a> {
+    fn new(writer: &'a mut io::Write) -> Paint<'a> {
+        Paint {
+            vec: Vec::new(),
+            writer,
+        }
+    }
+
+    fn push(&mut self, data: &[u8]) {
+        for elem in data {
+            self.vec.push(*elem);
+        }
+    }
+
+    fn push_one(&mut self, data: u8) {
+        self.vec.push(data);
+    }
+
+    fn write(&mut self) -> io::Result<usize> {
+        self.writer.write(&self.vec[..])
     }
 }
