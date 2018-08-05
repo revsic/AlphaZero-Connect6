d5b711951a93af32a148aa190775d75bda626da9
revsic <revsic99@gmail.com>
2018-08-06 05:58:36 +0900

    Update Simulate - Renew Simulate System, Remove Root

--- Connect6/src/mcts/simulate/mod.rs
+++ Connect6/src/mcts/simulate/mod.rs
@@ -1,167 +1,175 @@
 extern crate cpython;
 
 use cpython::*;
+use std::cell::RefCell;
+use std::rc::Rc;
 use super::super::game::*;
 
 #[cfg(test)]
 mod tests;
 
-pub struct Root {
-    pub turn: Player,
-    pub num_remain: i32,
+pub struct Node {
     pub board: [[Player; 19]; 19],
     pub possible: Vec<(usize, usize)>,
 }
 
-pub struct SimulateBack {
-    turn: Player,
-    num_remain: i32,
-    pos: Option<(usize, usize)>,
-    board: [[Player; 19]; 19],
-    possible: Vec<(usize, usize)>,
-}
-
-pub struct Simulate<'a> {
-    pub turn: Player,
-    pub num_remain: i32,
-    pub pos: Option<(usize, usize)>,
-    pub board: &'a mut [[Player; 19]; 19],
-    pub possible: &'a mut Vec<(usize, usize)>,
-}
-
-impl Root {
+impl Node {
+    #[inline]
     fn possible() -> Vec<(usize, usize)> {
-        (0..19).flat_map(|x| (0..19).map(move |y| (x, y)))
-               .collect()
+        (0..19).flat_map(|x| (0..19).map(move |y| (x, y))).collect()
     }
 
-    pub fn new() -> Root {
-        Root {
-            turn: Player::Black,
-            num_remain: 1,
+    fn new() -> Node {
+        Node {
             board: [[Player::None; 19]; 19],
-            possible: Self::possible()
+            possible: Self::possible(),
         }
     }
 
-    pub fn from_game(game: &Game) -> Root {
-        let board = game.get_board();
-        let possible = Self::possible().into_iter().filter(
-            |(r, c)| board[*r][*c] == Player::None
-        ).collect();
+    fn from_board(board: &[[Player; 19]; 19]) -> Node {
+        let possible = Self::possible()
+            .into_iter()
+            .filter(|(r, c)| board[*r][*c] == Player::None)
+            .collect();
 
-        Root {
-            turn: game.get_turn(),
-            num_remain: game.get_remain(),
-            board: *game.get_board(),
+        Node {
+            board: *board,
             possible,
         }
     }
+}
 
-    pub fn to_simulate(&mut self) -> Simulate {
+pub struct Simulate {
+    pub turn: Player,
+    pub num_remain: i32,
+    pub pos: Option<(usize, usize)>,
+    pub node: Rc<RefCell<Node>>,
+}
+
+impl Simulate {
+    pub fn new() -> Simulate {
+        Simulate {
+            turn: Player::Black,
+            num_remain: 1,
+            pos: None,
+            node: Rc::new(RefCell::new(Node::new())),
+        }
+    }
+
+    pub fn from_game(game: &Game) -> Simulate {
+        Simulate {
+            turn: game.get_turn(),
+            num_remain: game.get_remain(),
+            pos: None,
+            node: Rc::new(RefCell::new(Node::from_board(game.get_board()))),
+        }
+    }
+
+    pub fn deep_clone(&self) -> Simulate {
+        let node = self.node.borrow();
+        let board = node.board;
+
         Simulate {
             turn: self.turn,
             num_remain: self.num_remain,
             pos: None,
-            board: &mut self.board,
-            possible: &mut self.possible,
+            node: Rc::new(RefCell::new(Node::from_board(&board))),
         }
     }
-}
 
-impl<'a> Simulate<'a> {
+    pub fn board(&self) -> [[Player; 19]; 19] {
+        let node = self.node.borrow();
+        node.board
+    }
+
+    pub fn search_winner(&self) -> Player {
+        let board = &self.node.borrow().board;
+        search(board)
+    }
+
     pub fn validate(&self, row: usize, col: usize) -> bool {
         if row >= 19 || col >= 19 {
             return false;
         }
-        if self.board[row][col] != Player::None {
+        let board = &self.node.borrow().board;
+        if board[row][col] != Player::None {
             return false;
         }
         true
     }
 
-    pub fn is_game_end(&self) -> Player {
-        search(self.board)
-    }
+    pub fn simulate(&self, row: usize, col: usize) -> Simulate {
+        let mut node = self.node.borrow_mut();
+        let item = node.possible.iter()
+            .position(|x| *x == (row, col));
+        node.possible.remove(item.unwrap());
 
-    pub fn simulate(&mut self, row: usize, col: usize) -> Simulate {
-        let pos = (row, col);
-        let item = self.possible.iter()
-            .position(|x| *x == pos);
-        self.possible.remove(item.unwrap());
-
-        self.board[row][col] = self.turn;
-
-        let (turn, num_remain) = {
+        node.board[row][col] = self.turn;
+        let (turn, num_remain) =
             if self.num_remain <= 1 {
                 (self.turn.switch(), 2)
             } else {
                 (self.turn, 1)
-            }
-        };
+            };
 
         Simulate {
             turn,
             num_remain,
-            pos: Some(pos),
-            board: self.board,
-            possible: self.possible,
+            pos: Some((row, col)),
+            node: self.node.clone(),
         }
     }
 
-    pub fn simulate_mut(&mut self, row: usize, col: usize) {
-        let pos = (row, col);
-        let item = self.possible.iter()
-            .position(|x| *x == pos);
-        self.possible.remove(item.unwrap());
+    pub fn simulate_in(&mut self, row: usize, col: usize) {
+        let mut node = self.node.borrow_mut();
+        let item = node.possible.iter()
+            .position(|x| *x == (row, col));
+        node.possible.remove(item.unwrap());
 
-        self.board[row][col] = self.turn;
-
+        node.board[row][col] = self.turn;
         self.num_remain -= 1;
+
         if self.num_remain <= 0 {
             self.num_remain = 2;
             self.turn.mut_switch();
         }
     }
 
-    pub fn backup(&mut self) -> SimulateBack {
-        SimulateBack {
-            turn: self.turn,
-            num_remain: self.num_remain,
-            pos: self.pos,
-            board: *self.board,
-            possible: self.possible.clone(),
-        }
-    }
+    pub fn rollback_in(&mut self, row: usize, col: usize) {
+        let mut node = self.node.borrow_mut();
 
-    pub fn recover(&mut self, backup: SimulateBack) {
-        self.turn = backup.turn;
-        self.num_remain = backup.num_remain;
-        self.pos = backup.pos;
-        *self.board = backup.board;
-        *self.possible = backup.possible;
+        node.board[row][col] = Player::None;
+        node.possible.push((row, col));
+
+        self.num_remain += 1;
+        if self.num_remain > 2 {
+            self.num_remain = 1;
+            self.turn.mut_switch();
+        }
     }
 }
 
-impl<'a> Drop for Simulate<'a> {
+impl Drop for Simulate {
     fn drop(&mut self) {
         if let Some((row, col)) = self.pos {
-            self.possible.push((row, col));
-            self.board[row][col] = Player::None;
+            let mut node = self.node.borrow_mut();
+            node.possible.push((row, col));
+            node.board[row][col] = Player::None;
         }
     }
 }
 
-impl<'a> ToPyObject for Simulate<'a> {
+impl ToPyObject for Simulate {
     type ObjectType = PyTuple;
     fn to_py_object(&self, py: Python) -> Self::ObjectType {
+        let node = self.node.borrow();
         let player = (self.turn as i32).to_py_object(py).into_object();
         let num_remain = self.num_remain.to_py_object(py).into_object();
 
         let mut board: Vec<PyObject> = Vec::new();
         for i in 0..19 {
             for j in 0..19 {
-                board[i * 19 + j] = (self.board[i][j] as i32).to_py_object(py).into_object();
+                board[i * 19 + j] = (node.board[i][j] as i32).to_py_object(py).into_object();
             }
         }
         let list = PyList::new(py, board.as_slice()).into_object();
--- Connect6/src/mcts/simulate/tests.rs
+++ Connect6/src/mcts/simulate/tests.rs
@@ -3,117 +3,119 @@ extern crate rand;
 use super::*;
 
 #[cfg(test)]
-mod root_tests {
+mod simulate_tests {
     use super::*;
 
     #[test]
     fn test_new() {
-        let root = Root::new();
-        assert_eq!(root.turn, Player::Black);
-        assert_eq!(root.num_remain, 1);
-        assert_eq!(root.board, [[Player::None; 19]; 19]);
-        assert_eq!(root.possible.len(), 19 * 19);
+        let simulate = Simulate::new();
+        assert_eq!(simulate.turn, Player::Black);
+        assert_eq!(simulate.num_remain, 1);
+        assert_eq!(simulate.pos, None);
+
+        let node = simulate.node.borrow();
+        assert_eq!(node.board, [[Player::None; 19]; 19]);
+        assert_eq!(node.possible.len(), 19 * 19);
     }
 
     #[test]
     fn test_from_game() {
         let game = Game::new();
-        let root = Root::from_game(&game);
+        let simulate = Simulate::from_game(&game);
 
-        assert_eq!(game.get_turn(), root.turn);
-        assert_eq!(game.get_remain(), root.num_remain);
-        assert_eq!(*game.get_board(), root.board);
+        assert_eq!(simulate.turn, Player::Black);
+        assert_eq!(simulate.num_remain, 1);
+        assert_eq!(simulate.pos, None);
+
+        let node = simulate.node.borrow();
+        assert_eq!(node.board, [[Player::None; 19]; 19]);
+        assert_eq!(node.possible.len(), 19 * 19);
     }
 
     #[test]
-    fn test_to_simulate() {
-        let game = Game::new();
-        let mut root = Root::from_game(&game);
+    fn test_deep_clone() {
+        let simulate = Simulate::new();
+        let cloned = simulate.deep_clone();
 
-        let turn = root.turn;
-        let num_remain = root.num_remain;
-        {
-            let simulate = root.to_simulate();
-            assert_eq!(turn, simulate.turn);
-            assert_eq!(num_remain, simulate.num_remain);
-
-            simulate.board[0][0] = Player::White;
+        { // borrow_mut simulate.node: Rc<RefCell<Node>>
+            let mut node = simulate.node.borrow_mut();
+            node.board[0][0] = Player::Black;
         }
-        assert_eq!(root.board[0][0], Player::White);
+        assert_eq!(simulate.board()[0][0], Player::Black);
+        assert_eq!(cloned.board()[0][0], Player::None);
     }
-}
-
-#[cfg(test)]
-mod simulate_tests {
-    use super::*;
 
     #[test]
     fn test_validate() {
         let game = Game::new();
-        let mut root = Root::from_game(&game);
-
-        let simulate = root.to_simulate();
+        let simulate = Simulate::from_game(&game);
         assert!(simulate.validate(0, 0));
 
-        simulate.board[0][0] = Player::Black;
+        { // borrow_mut simulate.node: Rc<RefCell<Node>>
+            let mut node = simulate.node.borrow_mut();
+            node.board[0][0] = Player::Black;
+        }
         assert!(!simulate.validate(0, 0));
 
-        let row = rand::random::<usize>();
-        let col = rand::random::<usize>();
-
-        simulate.board[0][0] = Player::None;
+        let row = rand::random::<usize>() % 40 + 1;
+        let col = rand::random::<usize>() % 40 + 1;
         assert_eq!(simulate.validate(row, col), row < 19 && col < 19);
     }
 
     #[test]
     fn test_simulate() {
         let game = Game::new();
-        let mut root = Root::from_game(&game);
-        let mut simulate = root.to_simulate();
+        let simulate = Simulate::from_game(&game);
         {
-            let sim_aA = simulate.simulate(0, 0);
-            assert_eq!(sim_aA.board[0][0], Player::Black);
-            assert_eq!(sim_aA.turn, Player::White);
-            assert_eq!(sim_aA.num_remain, 2);
+            let sim_aa = simulate.simulate(0, 0);
+            let node = sim_aa.node.borrow();
+            assert_eq!(node.board[0][0], Player::Black);
+            assert_eq!(sim_aa.turn, Player::White);
+            assert_eq!(sim_aa.num_remain, 2);
 
-            let index = sim_aA.possible.iter().position(|x| *x == (0, 0));
+            let index = node.possible.iter().position(|x| *x == (0, 0));
             assert!(index.is_none());
         }
-        assert_eq!(simulate.board[0][0], Player::None);
+        let node = simulate.node.borrow();
+        assert_eq!(node.board[0][0], Player::None);
 
-        let index = simulate.possible.iter().position(|x| *x == (0, 0));
+        let index = node.possible.iter().position(|x| *x == (0, 0));
         assert!(index.is_some());
     }
 
     #[test]
-    fn test_simulate_mut() {
+    fn test_simulate_in() {
         let game = Game::new();
-        let mut root = Root::from_game(&game);
-        let mut simulate = root.to_simulate();
+        let mut simulate = Simulate::from_game(&game);
+        simulate.simulate_in(0, 0);
 
-        simulate.simulate_mut(0, 0);
-        assert_eq!(simulate.board[0][0], Player::Black);
+        let node = simulate.node.borrow();
+        assert_eq!(node.board[0][0], Player::Black);
         assert_eq!(simulate.turn, Player::White);
         assert_eq!(simulate.num_remain, 2);
 
-        let index = simulate.possible.iter().position(|x| *x == (0, 0));
+        let index = node.possible.iter().position(|x| *x == (0, 0));
         assert!(index.is_none());
     }
 
     #[test]
-    fn test_back_recover() {
+    fn test_rollback_in() {
         let game = Game::new();
-        let mut root = Root::from_game(&game);
-        let mut simulate = root.to_simulate();
+        let mut simulate = Simulate::from_game(&game);
 
-        let backup = simulate.backup();
-        simulate.simulate_mut(0, 0);
+        simulate.simulate_in(0, 0);
+        {
+            let node = simulate.node.borrow();
+            assert_eq!(node.board[0][0], Player::Black);
+        }
 
-        simulate.recover(backup);
-        assert_eq!(simulate.board[0][0], Player::None);
+        simulate.rollback_in(0, 0);
         assert_eq!(simulate.turn, Player::Black);
         assert_eq!(simulate.num_remain, 1);
-        assert!(simulate.pos.is_none());
-        assert_eq!(simulate.possible.len(), 19 * 19);
+        assert_eq!(simulate.pos, None);
+
+        let node = simulate.node.borrow();
+        assert_eq!(node.board, [[Player::None; 19]; 19]);
+        assert_eq!(node.possible.len(), 19 * 19);
     }
 }
