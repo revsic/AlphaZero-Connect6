d885ccabbe52ccdee66dd712503f189fa91dd5ce
revsic <revsic99@gmail.com>
2018-08-06 05:56:55 +0900

    Update Lib - Add Board Print Function

--- Connect6/src/lib.rs
+++ Connect6/src/lib.rs
@@ -17,3 +17,18 @@ py_module_initializer!(libconnect6, initlibconnect6, PyInit_connect6, |py, m| {
 fn test(_: Python, a: i64, b: i64) -> PyResult<i64> {
     Ok(a + b)
 }
+
+fn print_board(board: &[[game::Player; 19]; 19]) {
+    println!("0 A B C D E F G H I J K L M N O P Q R S");
+    for i in 0..19 {
+        print!("{} ", (0x61 + i as u8) as char);
+        for j in 0..19 {
+            match board[i][j] {
+                game::Player::Black => print!("X "),
+                game::Player::White => print!("O "),
+                game::Player::None => print!("_ "),
+            }
+        }
+        println!();
+    }
+}
