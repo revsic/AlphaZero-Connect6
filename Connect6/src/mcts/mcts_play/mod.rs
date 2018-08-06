247dfa4b905a01e105b4ab96160224c608313b43
revsic <revsic99@gmail.com>
2018-08-07 03:56:53 +0900

    Update MCTS Play - Implement ToPyObject for Path and RunResult

--- Connect6/src/mcts/mcts_play/mod.rs
+++ Connect6/src/mcts/mcts_play/mod.rs
@@ -1,52 +1,70 @@
+extern crate cpython;
+
+use cpython::*;
+use std::io;
+
 use super::*;
 use super::super::game::*;
 use super::super::agent::*;
+use super::super::pybind::*;
 
 #[cfg(test)]
 mod tests;
 
-struct Path {
-    turn: Player,
-    pos: (usize, usize),
+pub struct Path {
+    pub turn: Player,
+    pub board: [[Player; 19]; 19],
+    pub pos: (usize, usize),
 }
 
-struct RunResult {
-    winner: Player,
-    path: Vec<Path>,
+pub struct RunResult {
+    pub winner: Player,
+    pub path: Vec<Path>,
 }
 
-struct SinglePolicyMCTS<'a, P> where P: 'a + Policy + Sized {
+pub struct SinglePolicyMCTS<'a, P> where P: 'a + Policy + Sized {
     policy: &'a mut P,
+    debug: bool,
 }
 
-struct SeperatePolicyMCTS<'a, 'b, P, Q>
+pub struct SeperatePolicyMCTS<'a, 'b, P, Q>
     where P: 'a + Policy + Sized,
           Q: 'b + Policy + Sized {
     black_policy: &'a mut P,
     white_policy: &'b mut Q,
+    debug: bool,
 }
 
 impl<'a, P> SinglePolicyMCTS<'a, P> where P: 'a + Policy + Sized {
-    fn new(policy: &'a mut P,) -> SinglePolicyMCTS<'a, P> {
+    pub fn new(policy: &'a mut P,) -> SinglePolicyMCTS<'a, P> {
         SinglePolicyMCTS {
             policy,
+            debug: false,
         }
     }
 
-    fn run(&mut self) -> RunResult {
+    pub fn debug(policy: &'a mut P) -> SinglePolicyMCTS<'a, P> {
+        SinglePolicyMCTS {
+            policy,
+            debug: true,
+        }
+    }
+
+    pub fn run(&mut self) -> RunResult {
         let mut winner = Player::None;
         let agent = Agent::with_start();
         let game = agent.get_game();
 
         let mut path = Vec::new();
         loop {
-            let (turn, (row, col)) = {
+            let (turn, board, (row, col)) = {
                 let game = game.read().unwrap();
                 let turn = game.get_turn();
-                (turn, self.policy.get_policy(&*game))
+                if self.debug { game.print(&mut io::stdout()).unwrap(); }
+                (turn, *game.get_board(), self.policy.get_policy(&*game))
             };
 
-            path.push(Path{ turn, pos: (row, col) });
+            path.push(Path{ turn, board, pos: (row, col) });
             let row = (row as u8 + 0x61) as char;
             let col = (col as u8 + 0x41) as char;
             let query: String = vec![row, col].iter().collect();
@@ -69,21 +87,30 @@ impl<'a, 'b, P, Q> SeperatePolicyMCTS<'a, 'b, P, Q>
     where P: 'a + Policy + Sized,
           Q: 'b + Policy + Sized
 {
-    fn new(black_policy: &'a mut P, white_policy: &'b mut Q) -> SeperatePolicyMCTS<'a, 'b, P, Q> {
+    pub fn new(black_policy: &'a mut P, white_policy: &'b mut Q) -> SeperatePolicyMCTS<'a, 'b, P, Q> {
         SeperatePolicyMCTS {
             black_policy,
             white_policy,
+            debug: false,
         }
     }
 
-    fn run(&mut self) -> RunResult {
+    pub fn debug(black_policy: &'a mut P, white_policy: &'b mut Q) -> SeperatePolicyMCTS<'a, 'b, P, Q> {
+        SeperatePolicyMCTS {
+            black_policy,
+            white_policy,
+            debug: true,
+        }
+    }
+
+    pub fn run(&mut self) -> RunResult {
         let mut winner = Player::None;
         let agent = Agent::with_start();
         let game = agent.get_game();
 
         let mut path = Vec::new();
         loop {
-            let (turn, (row, col)) = {
+            let (turn, board, (row, col)) = {
                 let game = game.read().unwrap();
                 let turn = game.get_turn();
                 let pos = match turn {
@@ -92,10 +119,11 @@ impl<'a, 'b, P, Q> SeperatePolicyMCTS<'a, 'b, P, Q>
                     Player::Black => self.black_policy.get_policy(&*game),
                     Player::White => self.white_policy.get_policy(&*game),
                 };
-                (turn, pos)
+                if self.debug { game.print(&mut io::stdout()).unwrap(); }
+                (turn, *game.get_board(), pos)
             };
 
-            path.push(Path{ turn, pos: (row, col) });
+            path.push(Path{ turn, board, pos: (row, col) });
             let row = (row as u8 + 0x61) as char;
             let col = (col as u8 + 0x41) as char;
             let query: String = vec![row, col].iter().collect();
@@ -111,5 +139,34 @@ impl<'a, 'b, P, Q> SeperatePolicyMCTS<'a, 'b, P, Q>
         }
 
         RunResult { winner, path }
+    }
+}
+
+impl ToPyObject for Path {
+    type ObjectType = PyTuple;
+    fn to_py_object(&self, py: Python) -> PyTuple {
+        let turn = (self.turn as i32).to_py_object(py).into_object();
+        let board = pylist_from_board(py, &self.board);
+        let (row, col) = self.pos;
+
+        let row = (row as i32).to_py_object(py).into_object();
+        let col = (col as i32).to_py_object(py).into_object();
+        let pos_tuple = PyTuple::new(py, &[row, col]).into_object();
+
+        let tuple = PyTuple::new(py, &[turn, board, pos_tuple]);
+        tuple
+    }
+}
+
+impl ToPyObject for RunResult {
+    type ObjectType = PyTuple;
+    fn to_py_object(&self, py: Python) -> PyTuple {
+        let win = (self.winner as i32).to_py_object(py).into_object();
+        let path = self.path.iter()
+            .map(|x| x.to_py_object(py).into_object())
+            .collect::<Vec<_>>();
+        let list = PyList::new(py, path.as_slice()).into_object();
+        let tuple = PyTuple::new(py, &[win, list]);
+        tuple
     }
 }
