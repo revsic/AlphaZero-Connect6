6a07a8cde7ee3f65ae35249ff8685eafba9c8bd7
revsic <revsic99@gmail.com>
2018-08-10 06:23:42 +0900

    Update WeightedPolicy - Simple Linearly Weighted Policy

---
+++ AlphaZero/weighted/model.py
@@ -0,0 +1,93 @@
+import json
+import tensorflow as tf
+
+
+class WeightedPolicy(object):
+    """ WeightedPolicy
+    Attributes:
+        sess: tf.Session, sesion for inference policy
+        board_size: int, size of the board
+        learning_rate: Float, learning rate.
+        momentum: float, momentum for the optimizer.
+        plc_x: tf.placeholder, input vector, flattened board.
+        plc_value: tf.placeholder, expected value vector.
+        plc_policy: tf.placeholder, policy based on monte carlo tree search
+        value: tf.Tensor, value head of the model
+        policy: tf.Tensor, policy head of the model
+        loss: tf.Tensor, loss function.
+        optimize: tf.Tensor, optimize object.
+        summary: tf.Tensor, tensor summary of the metric.
+    """
+    def __init__(self, sess, board_size, learning_rate, momentum):
+        """ Initializer
+        Args:
+            sess: tf.Session, session for inference policy
+            board_size: int, size of the board.
+            learning_rate: Float, learning rate.
+            momentum: float, momentum for the optimizer.
+        """
+        self.sess = sess
+        self.board_size = board_size
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+
+        board_capacity = board_size ** 2
+        self.plc_x = tf.placeholder(tf.float32, [None, board_capacity])
+
+        self.plc_value = tf.placeholder(tf.float32, [None])
+        self.plc_policy = tf.placeholder(tf.int32, [None])
+
+        self.value, self.policy = self._get_model()
+        self.output_policy = tf.nn.softmax(self.policy)
+        self.value_loss, self.policy_loss = self._get_loss()
+        self.loss = self.value_loss + self.policy_loss
+
+        self.optimize = tf.train.MomentumOptimizer(self.learning_rate, self.momentum).minimize(self.loss)
+        self.summary = tf.summary.merge([
+            tf.summary.scalar('value_mse', self.value_loss),
+            tf.summary.scalar('policy_ce', self.policy_loss),
+            tf.summary.scalar('loss', self.loss)
+        ])
+        self.ckpt = tf.train.Saver()
+
+    def __call__(self, board, **kwargs):
+        return self.sess.run((self.value, self.output_policy), feed_dict={self.plc_x: board})
+
+    def train(self, x, value, policy):
+        self.sess.run(self.optimize, feed_dict={self.plc_x: x, self.plc_value: value, self.plc_policy: policy})
+
+    def inference(self, obj, x, value, policy):
+        return self.sess.run(obj, feed_dict={self.plc_x: x, self.plc_value: value, self.plc_policy: policy})
+
+    def dump(self, path):
+        self.ckpt.save(self.sess, path + '.ckpt')
+        with open(path + '.json', 'w') as f:
+            dump = json.dumps(
+                {
+                    'board_size': self.board_size,
+                    'learning_rate': self.learning_rate,
+                    'momentum': self.momentum
+                }
+            )
+            f.write(dump)
+
+    @classmethod
+    def load(cls, sess, path):
+        with open(path + '.json') as f:
+            param = json.loads(f.read())
+
+        model = cls(sess, param['board_size'], param['learning_rate'], param['momentum'])
+        model.ckpt.restore(sess, path + '.ckpt')
+
+        return model
+
+    def _get_model(self):
+        policy = tf.layers.dense(self.plc_x, self.board_size ** 2)
+        value = tf.layers.dense(self.plc_x, 1, activation=tf.nn.tanh)
+        return tf.reshape(value, (-1, )), policy  # value, policy
+
+    def _get_loss(self):
+        value_loss = tf.reduce_sum(tf.square(self.plc_value - self.value))
+        policy = tf.one_hot(self.plc_policy, self.board_size ** 2)
+        policy_loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits_v2(labels=policy, logits=self.policy))
+        return value_loss, policy_loss
---
+++ AlphaZero/weighted/train.py
@@ -0,0 +1,65 @@
+import os
+import numpy as np
+import tensorflow as tf
+from datetime import datetime
+
+import env
+from buffer import Buffer
+from weighted.model import WeightedPolicy
+
+flags = tf.app.flags
+flags.DEFINE_float('learning_rate', 1e-3, 'float, learning rate, default 1e-3.')
+flags.DEFINE_float('momentum', 0.9, 'float, beta1 value in Adam, default 0.9.')
+flags.DEFINE_integer('board_size', 15, 'int, size of the board, default 15')
+flags.DEFINE_integer('max_buffer', 10000, 'int, max size of buffer, default 100000')
+flags.DEFINE_integer('start_train', 2000, 'int, start train when the size of buffer over given, default 60000')
+flags.DEFINE_integer('batch_size', 1, 'int, size of batch, default 32')
+flags.DEFINE_integer('mini_batch', 1024, 'int, size of mini-batch, default 2048.')
+flags.DEFINE_integer('ckpt_interval', 100, 'int, interval for writing checkpoint, default 5')
+flags.DEFINE_string('name', 'default', 'String, name of model, default `default`.')
+flags.DEFINE_string('summary_dir', '.\weighted\summary', 'String, dir name for saving tensor summary, default `./summary`.')
+flags.DEFINE_string('ckpt_dir', '.\weighted\ckpt', 'String, dir name for saving checkpoint, default `./ckpt_dir`.')
+FLAGS = flags.FLAGS
+
+def log(msg):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print('[{}] {}'.format(timestamp, msg))
+
+def main(_):
+    ckpt_path = os.path.join(FLAGS.ckpt_dir, FLAGS.name)
+    buffer = Buffer(FLAGS.max_buffer, FLAGS.board_size, FLAGS.mini_batch)
+    with tf.Session() as sess:
+        policy = WeightedPolicy(sess, FLAGS.board_size, FLAGS.learning_rate, FLAGS.momentum)
+        writer = tf.summary.FileWriter(os.path.join(FLAGS.summary_dir, FLAGS.name), sess.graph)
+
+        param = env.default_param()
+        param['num_simulation'] = 500
+
+        epoch = 0
+        num_game = 0
+        sess.run(tf.global_variables_initializer())
+        while True:
+            num_game += 1
+            res = env.with_param(policy, param)
+            buffer.push_game(res)
+
+            log('self-play game#{}'.format(num_game))
+            if len(buffer) > FLAGS.start_train:
+                epoch += 1
+                for _ in range(FLAGS.batch_size):
+                    value, board, pos = buffer.sample()
+                    policy.train(board, value, pos)
+
+                value, board, pos = buffer.sample()
+                summary = policy.inference(policy.summary, board, value, pos)
+                writer.add_summary(summary, global_step=epoch)
+
+                if epoch % FLAGS.ckpt_interval == 0:
+                    policy.dump(ckpt_path)
+                    log('ckpt saved')
+
+                log('epoch#{}'.format(epoch))
+
+
+if __name__ == '__main__':
+    tf.app.run()
