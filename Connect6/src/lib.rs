d4da0b716cb1d3c430772329c2dfecfae80839f5
revsic <revsic99@gmail.com>
2018-08-14 21:46:38 +0900

    Update Lib - Remove with_param

--- Connect6/src/lib.rs
+++ Connect6/src/lib.rs
@@ -15,44 +15,32 @@ type Board = [[game::Player; BOARD_SIZE]; BOARD_SIZE];
 
 py_module_initializer!(libconnect6, initlibconnect6, PyInit_connect6, |py, m| {
     try!(m.add(py, "__doc__", "This module is implemented in Rust, for Simulating Connect6"));
-    try!(m.add(py, "self_play", py_fn!(py, self_play(object: PyObject))));
-    try!(m.add(py, "play_with", py_fn!(py, play_with(object: PyObject))));
-    try!(m.add(py, "with_param", py_fn!(py, with_param(object: PyObject,
-                                                       num_simulation: i32,
-                                                       num_expansion: usize,
-                                                       epsilon: f32,
-                                                       dirichlet_alpha: f64,
-                                                       c_puct: f32,
-                                                       debug: bool,
-                                                       num_game_thread: i32))));
+    try!(m.add(py, "self_play", py_fn!(py, self_play(object: PyObject,
+                                                     num_simulation: i32,
+                                                     num_expansion: usize,
+                                                     epsilon: f32,
+                                                     dirichlet_alpha: f64,
+                                                     c_puct: f32,
+                                                     debug: bool,
+                                                     num_game_thread: i32))));
+    try!(m.add(py, "play_with", py_fn!(py, play_with(object: PyObject,
+                                                     num_simulation: i32,
+                                                     num_expansion: usize,
+                                                     epsilon: f32,
+                                                     dirichlet_alpha: f64,
+                                                     c_puct: f32))));
     Ok(())
 });
 
-fn self_play(py: Python, object: PyObject) -> PyResult<PyTuple> {
-    let mut policy = pybind::AlphaZero::new(object);
-    let result = agent::Agent::new(&mut policy).play();
-    Ok(result.unwrap().to_py_object(py))
-}
-
-fn play_with(py: Python, object: PyObject) -> PyResult<PyTuple> {
-    let mut stdin = std::io::stdin();
-    let mut stdout = std::io::stdout();
-    let mut io_policy = policy::IoPolicy::new(&mut stdin, &mut stdout);
-    let mut py_policy = pybind::AlphaZero::new(object);
-    let mut multi_policy = policy::MultiPolicy::new(&mut py_policy, &mut io_policy);
-    let result = agent::Agent::new(&mut multi_policy).play();
-    Ok(result.unwrap().to_py_object(py))
-}
-
-fn with_param(py: Python,
-              object: PyObject,
-              num_simulation: i32,
-              num_expansion: usize,
-              epsilon: f32,
-              dirichlet_alpha: f64,
-              c_puct: f32,
-              debug: bool,
-              num_game_thread: i32) -> PyResult<PyTuple> {
+fn self_play(py: Python,
+             object: PyObject,
+             num_simulation: i32,
+             num_expansion: usize,
+             epsilon: f32,
+             dirichlet_alpha: f64,
+             c_puct: f32,
+             debug: bool,
+             num_game_thread: i32) -> PyResult<PyTuple> {
     let param = pybind::HyperParameter {
         num_simulation,
         num_expansion,
@@ -63,7 +51,8 @@ fn with_param(py: Python,
     if num_game_thread == 1 {
         let mut policy = pybind::AlphaZero::with_param(object, param);
         let result =
-            if debug { agent::Agent::new(&mut policy).play() } else { agent::Agent::debug(&mut policy).play() };
+            if debug { agent::Agent::new(&mut policy).play() }
+                else { agent::Agent::debug(&mut policy).play() };
         Ok(result.unwrap().to_py_object(py))
     } else {
         let result = py.allow_threads(move || {
@@ -76,15 +65,38 @@ fn with_param(py: Python,
                 pybind::AlphaZero::with_param(object, param)
             };
             let async_agent =
-                if debug { agent::AsyncAgent::new(policy_gen) } else { agent::AsyncAgent::debug(policy_gen) };
-            let result = async_agent.run(num_game_thread);
-
-            let gil = Python::acquire_gil();
-            let py = gil.python();
-            result.iter()
-                .map(|x| x.to_py_object(py).into_object())
-                .collect::<Vec<_>>()
+                if debug { agent::AsyncAgent::new(policy_gen) }
+                    else { agent::AsyncAgent::debug(policy_gen) };
+            async_agent.run(num_game_thread)
         });
-        Ok(PyTuple::new(py, result.as_slice()))
+        let py_result = result.iter()
+            .map(|x| x.to_py_object(py).into_object())
+            .collect::<Vec<_>>();
+        Ok(PyTuple::new(py, py_result.as_slice()))
     }
+}
+
+fn play_with(py: Python,
+             object: PyObject,
+             num_simulation: i32,
+             num_expansion: usize,
+             epsilon: f32,
+             dirichlet_alpha: f64,
+             c_puct: f32) -> PyResult<PyTuple> {
+    let param = pybind::HyperParameter {
+        num_simulation,
+        num_expansion,
+        epsilon,
+        dirichlet_alpha,
+        c_puct,
+    };
+    let mut py_policy = pybind::AlphaZero::with_param(object, param);
+
+    let mut stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+    let mut io_policy = policy::IoPolicy::new(&mut stdin, &mut stdout);
+
+    let mut multi_policy = policy::MultiPolicy::new(&mut py_policy, &mut io_policy);
+    let result = agent::Agent::debug(&mut multi_policy).play();
+    Ok(result.unwrap().to_py_object(py))
 }
