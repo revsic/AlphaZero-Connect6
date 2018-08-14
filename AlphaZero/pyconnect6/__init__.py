db23724164c8fb11924b3dbecf062a57e71be12f
revsic <revsic99@gmail.com>
2018-08-14 21:47:12 +0900

    Update PyConnect6 - Adjust for New Connect6 Front

--- AlphaZero/pyconnect6/__init__.py
+++ AlphaZero/pyconnect6/__init__.py
@@ -1,12 +1,17 @@
+import json
 import pyconnect6.connect6
 
 
-def self_play(policy):
-    return connect6.self_play(policy)
+def self_play(policy, param=None):
+    if param is None:
+        param = default_param()
+    return connect6.self_play(policy, *param_to_tuple(param))
 
 
-def play_with(policy):
-    return connect6.play_with(policy)
+def play_with(policy, param=None):
+    if param is None:
+        param = default_param()
+    return connect6.play_with(policy, *param_to_tuple(param)[:-2])
 
 
 def default_param():
@@ -21,12 +26,24 @@ def default_param():
     }
 
 
-def with_param(policy, param):
-    return connect6.with_param(policy,
-                              param['num_simulation'],
-                              param['num_expansion'],
-                              param['epsilon'],
-                              param['dirichlet_alpha'],
-                              param['c_puct'],
-                              param['debug'],
-                              param['num_game_thread'])
+def param_to_tuple(param):
+    return param['num_simulation'],\
+           param['num_expansion'],\
+           param['epsilon'],\
+           param['dirichlet_alpha'],\
+           param['c_puct'],\
+           param['debug'],\
+           param['num_game_thread']
+
+
+def dump_param(path, param=None):
+    if param is None:
+        param = default_param()
+    with open(path + '_mcts.json', 'w') as f:
+        dump = json.dumps(param)
+        f.write(dump)
+
+
+def load_param(path):
+    with open(path + '_mcts.json') as f:
+        return json.loads(f.read())
