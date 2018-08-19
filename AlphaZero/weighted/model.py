5b9cf5d8b3dc799aabd03617e7363adb397c1efe
revsic <revsic99@gmail.com>
2018-08-20 02:43:57 +0900

    Update Weighted - Fix weighted.__call__ get turn and board

--- AlphaZero/weighted/model.py
+++ AlphaZero/weighted/model.py
@@ -3,20 +3,22 @@ import tensorflow as tf
 
 
 class WeightedPolicy(object):
-    """ WeightedPolicy
+    """ Weighted Policy consists of single fully connected layer
+
     Attributes:
         sess: tf.Session, sesion for inference policy
         board_size: int, size of the board
-        learning_rate: Float, learning rate.
+        learning_rate: float, learning rate.
         momentum: float, momentum for the optimizer.
-        plc_x: tf.placeholder, input vector, flattened board.
+        plc_player: tf.placeholder, player of the given board status.
+        plc_board: tf.placeholder, input vector, flattened board.
         plc_value: tf.placeholder, expected value vector.
         plc_policy: tf.placeholder, policy based on monte carlo tree search
         value: tf.Tensor, value head of the model
         policy: tf.Tensor, policy head of the model
         loss: tf.Tensor, loss function.
         optimize: tf.Tensor, optimize object.
-        summary: tf.Tensor, tensor summary of the metric.
+        summary: tf.Tensor, tensor summary.
     """
     def __init__(self, sess, board_size, learning_rate, momentum):
         """ Initializer
@@ -32,7 +34,8 @@ class WeightedPolicy(object):
         self.momentum = momentum
 
         board_capacity = board_size ** 2
-        self.plc_x = tf.placeholder(tf.float32, [None, board_capacity])
+        self.plc_player = tf.placeholder(tf.float32, [None])
+        self.plc_board = tf.placeholder(tf.float32, [None, board_capacity])
 
         self.plc_value = tf.placeholder(tf.float32, [None])
         self.plc_policy = tf.placeholder(tf.int32, [None])
@@ -50,14 +53,24 @@ class WeightedPolicy(object):
         ])
         self.ckpt = tf.train.Saver()
 
-    def __call__(self, board, **kwargs):
-        return self.sess.run((self.value, self.output_policy), feed_dict={self.plc_x: board})
+    def __call__(self, turn, board, **kwargs):
+        return self.sess.run((self.value, self.output_policy),
+                             feed_dict={self.plc_player: [turn] * len(board),
+                                        self.plc_board: board})
 
-    def train(self, x, value, policy):
-        self.sess.run(self.optimize, feed_dict={self.plc_x: x, self.plc_value: value, self.plc_policy: policy})
+    def train(self, turn, board, value, policy):
+        self.sess.run(self.optimize,
+                      feed_dict={self.plc_player: turn,
+                                 self.plc_board: board,
+                                 self.plc_value: value,
+                                 self.plc_policy: policy})
 
-    def inference(self, obj, x, value, policy):
-        return self.sess.run(obj, feed_dict={self.plc_x: x, self.plc_value: value, self.plc_policy: policy})
+    def inference(self, obj, turn, board, value, policy):
+        return self.sess.run(obj,
+                             feed_dict={self.plc_player: turn,
+                                        self.plc_board: board,
+                                        self.plc_value: value,
+                                        self.plc_policy: policy})
 
     def dump(self, path):
         self.ckpt.save(self.sess, path + '.ckpt')
@@ -82,12 +95,15 @@ class WeightedPolicy(object):
         return model
 
     def _get_model(self):
-        policy = tf.layers.dense(self.plc_x, self.board_size ** 2)
-        value = tf.layers.dense(self.plc_x, 1, activation=tf.nn.tanh)
+        player = tf.reshape(self.plc_player, (-1, 1))
+        repr = tf.concat([player, self.plc_board], axis=1)
+
+        policy = tf.layers.dense(repr, self.board_size ** 2)
+        value = tf.layers.dense(repr, 1, activation=tf.nn.tanh)
         return tf.reshape(value, (-1, )), policy  # value, policy
 
     def _get_loss(self):
         value_loss = tf.reduce_mean(tf.square(self.plc_value - self.value))
-        policy = tf.one_hot(self.plc_policy, self.board_size ** 2)
-        policy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=policy, logits=self.policy))
+        policy_loss = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.plc_policy, logits=self.policy))
         return value_loss, policy_loss
