0c1b967e13001f9a2163c9edb9a3c2c1c2be3ec5
revsic <revsic99@gmail.com>
2018-08-20 02:23:50 +0900

    Update RandomPolicy - Fix RandomPolicy.__call__ get parameter turn

--- AlphaZero/pyconnect6/random_policy.py
+++ AlphaZero/pyconnect6/random_policy.py
@@ -8,15 +8,16 @@ board_size = 15
 class RandomPolicy:
     """ Random Policy for introducing pyconnect6 usage.
 
-    Policy must have method `__call__(self, board): (value, prob)`,
+    Policy must have method `__call__(self, turn, board): (value, prob)`,
         where board is num_simulation by board_capacity size 2D list contains integers,
         where board_capacity = board_size ** 2
+    turn represent current turn, { -1: Black, 0: None, 1: White }
     `board[i]` represent board states and each cell represent { -1: Black Stone, 0: Empty Cell, 1: White Stone }
     `value` is `num_simulation` size list contains floats, represent probability of winning.
     `prob` is num_simulation by board_capacity size 2D list contains floats,
         represent probability of choosing each cell.
     """
-    def __call__(self, board):
+    def __call__(self, turn, board):
         size = len(board)
         value = np.random.rand(size)
         rand_policy = np.random.rand(size, board_size * board_size)
