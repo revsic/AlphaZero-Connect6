81f1140fabe26a2af9ee5607049ed6ca9b8fc960
revsic <revsic99@gmail.com>
2018-08-22 05:04:05 +0900

    Update Doc - Add PyBind Impl doc

--- Connect6/src/pybind/mod.rs
+++ Connect6/src/pybind/mod.rs
@@ -1,3 +1,15 @@
+//! Rust bindings for python interface and impl of AlphaZero policy
+//!
+//! It provides `rust-cpython` based bindings of some utilities to implement policy `AlphaZero`.
+//! `AlphaZero` policy is implemented based on [Mastering the game of Go with deep neural networks and tree search](https://www.nature.com/articles/nature16961)
+//! and [Mastering Chess and Shogi by Self-Play with a General Reinforcement Learning Algorithm](https://arxiv.org/abs/1712.01815).
+//! It pass callable python object with method `__call__(self, turn, board): (value, prob)`
+//! and make decision with combined mcts and value, probability approximator.
+//!
+//! # Examples
+//! ```rust
+//!
+//! ```
 pub use self::py_policy::*;
 pub use self::pybind_impl::*;
 
