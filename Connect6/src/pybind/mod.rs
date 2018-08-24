e26a82e10a5f29eda0333509d604960a5ce72263
revsic <revsic99@gmail.com>
2018-08-25 03:34:09 +0900

    Update PyBind - Split macro

--- Connect6/src/pybind/mod.rs
+++ Connect6/src/pybind/mod.rs
@@ -1,21 +1,7 @@
 //! Rust bindings for python interface and impl of AlphaZero policy
 //!
 //! It provides `rust-cpython` based bindings of some utilities to implement policy `AlphaZero`.
-//! `AlphaZero` policy is implemented based on [Mastering the game of Go with deep neural networks and tree search](https://www.nature.com/articles/nature16961)
-//! and [Mastering Chess and Shogi by Self-Play with a General Reinforcement Learning Algorithm](https://arxiv.org/abs/1712.01815).
-//! It pass callable python object with method `__call__(self, turn, board): (value, prob)`
-//! and make decision with combined mcts and value, probability approximator.
 //!
-//! # Examples
-//! ```rust
-//! // python : pyobj = lambda t, b: (np.random.rand(len(b)), np.random.rand(len(b), board_size ** 2))
-//! let mut policy = AlphaZero::new(pyobj);
-//! let result = Agent::new(&mut policy).play();
-//! assert!(result.is_ok());
-//! ```
-pub use self::py_policy::*;
 pub use self::pybind_impl::*;
 
-#[macro_use]
 mod pybind_impl;
-mod py_policy;
--- Connect6/src/pybind/pybind_impl/mod.rs
+++ Connect6/src/pybind/pybind_impl/mod.rs
@@ -10,22 +10,6 @@ use super::super::{BOARD_SIZE, Board};
 #[cfg(test)]
 mod tests;
 
-/// Panic with given error string if given Result is Err
-///
-/// # Examples
-/// ```rust
-/// let pyseq = pycheck!(obj.cast_into::<PySequence>(py), "couldn't cast obj into PySequence");
-/// ```
-#[macro_export]
-macro_rules! pycheck {
-    ($e:expr, $err:expr) => {
-        match $e {
-            Ok(obj) => obj,
-            Err(e) => panic!("{} : {:?}", $err, e),
-        }
-    }
-}
-
 /// Convert PySequence to Vec<f32>
 ///
 /// # Examples
@@ -39,8 +23,8 @@ macro_rules! pycheck {
 /// - If casted `obj` couldn't generate `PyIterator`.
 /// ```
 pub fn pyseq_to_vec(py: Python, obj: PyObject) -> Option<Vec<f32>> {
-    let pyseq = pycheck!(obj.cast_into::<PySequence>(py), "pyseq_to_vec couldn't cast obj into pyseq");
-    let pyiter = pycheck!(pyseq.iter(py), "pyseq_to_vec couldn't get iter from pyseq");
+    let pyseq = must!(obj.cast_into::<PySequence>(py), "pyseq_to_vec couldn't cast obj into pyseq");
+    let pyiter = must!(pyseq.iter(py), "pyseq_to_vec couldn't get iter from pyseq");
     let vec = pyiter
         .filter_map(|x| x.ok())
         .filter_map(|x| x.extract::<f32>(py).ok())
