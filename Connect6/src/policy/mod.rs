23644b29d6385fb7bec21419ae1cbe1bcf19b8e5
revsic <revsic99@gmail.com>
2018-08-27 05:26:00 +0900

    Update Doc - Update Policy

--- Connect6/src/policy/mod.rs
+++ Connect6/src/policy/mod.rs
@@ -1,10 +1,10 @@
 //! Predefined policy for agent to play game.
 //!
-//! Policy represents alogirthm that makes choice in given situation.
-//! This module provides 4 predefined policies, random policy, io policy, multi-policy and default MCTS policy.
+//! Policy represents alogirthm that make choice in given situation.
+//! This module provides 5 predefined policies, AlphaZero policy, random policy, io policy, multi-policy and pure MCTS policy.
 //!
 //! - Policy : trait for playing game with `Agent`.
-//! - AlphaZero : implementation of policy [Mastering the game of Go with deep neural networks and tree search](https://www.nature.com/articles/nature16961).
+//! - AlphaZero : implementation of policy [AlphaZero](https://arxiv.org/abs/1712.01815).
 //! - RandomPolicy : select possible position with randomness.
 //! - IoPolicy : read user input.
 //! - MultiPolicy : Black-White seperable policy, pass two different policies as initialize parameter.
@@ -14,11 +14,14 @@
 //! ```rust
 //! struct zero_policy {}
 //! impl Policy for zero_policy {
-//!     fn next(&mut self, game: &Game) -> Option<(usize, usize)> { Some((0, 0)) }
+//!     fn next(&mut self, game: &Game) -> Option<(usize, usize)> {
+//!         Some((0, 0))
+//!     }
 //! }
+//!
 //! let mut policy = zero_policy {};
 //! let result = Agent::new(&mut policy).play();
-//! assert_eq!(result, Err(String::from("game::play already set position")));
+//! assert!(result.is_err());
 //! ```
 pub use self::simulate::*;
 pub use self::alphazero_policy::*;
@@ -36,8 +39,8 @@ mod simulate;
 
 use super::game::Game;
 
-/// trait for playing game with `Agent`.
+/// trait for playing game with Agent.
 pub trait Policy {
-    /// get next selection
+    /// generate next selection
     fn next(&mut self, game: &Game) -> Option<(usize, usize)>;
 }
