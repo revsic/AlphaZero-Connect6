75330e579847b8cadefb4af3278efe411cee8595
revsic <revsic99@gmail.com>
2018-08-27 06:31:30 +0900

    Update Doc - Update PyBind

--- Connect6/src/pybind/mod.rs
+++ Connect6/src/pybind/mod.rs
@@ -1,6 +1,6 @@
 //! Rust bindings for python interface and impl of AlphaZero policy
 //!
-//! It provides `rust-cpython` based bindings of some utilities to implement policy `AlphaZero`.
+//! It provides `rust-cpython` based bindings for some utilities to implement policy `AlphaZero`.
 //!
 pub use self::pybind_impl::*;
 
