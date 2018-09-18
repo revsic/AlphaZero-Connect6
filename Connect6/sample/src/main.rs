255c94059616c7383c876c931c35a9319647035d
revsic <revsic99@gmail.com>
2018-09-18 17:26:06 +0900

    Update Connect6 Sample - Rust Sample of Connect6

---
+++ Connect6/sample/src/main.rs
@@ -0,0 +1,23 @@
+extern crate connect6;
+
+use connect6::{agent, policy, game::Player};
+use std::time::Instant;
+
+fn main() {
+    let rand_policy = || policy::RandomPolicy::new();
+    let async_agent = agent::AsyncAgent::debug(rand_policy);
+
+    let now = Instant::now();
+    let result = async_agent.run(4);
+    let elapsed = now.elapsed();
+    println!("{}.{}s elapsed", elapsed.as_secs(), elapsed.subsec_millis());
+
+    assert_eq!(result.len(), 4);
+    for run_result in result {
+        if let Some(last) = run_result.path.last() {
+            if run_result.winner != Player::None {
+                assert_eq!(last.turn, run_result.winner);
+            }
+        }
+    }
+}
