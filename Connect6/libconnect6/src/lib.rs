b4607fe3e729d645a25f440f695c9ef593c5d4e7
revsic <revsic99@gmail.com>
2018-09-18 20:10:10 +0900

    Update Lib - Apply rustfmt

--- Connect6/libconnect6/src/lib.rs
+++ Connect6/libconnect6/src/lib.rs
@@ -49,19 +49,41 @@ const BOARD_CAPACITY: usize = BOARD_SIZE * BOARD_SIZE;
 type Board = [[game::Player; BOARD_SIZE]; BOARD_SIZE];
 
 py_module_initializer!(libconnect6, initlibconnect6, PyInit_connect6, |py, m| {
-    try!(m.add(py, "__doc__", "This module is implemented in Rust, for Simulating Connect6"));
-    try!(m.add(py, "self_play", py_fn!(py, self_play(object: PyObject,
-                                                     num_simulation: i32,
-                                                     epsilon: f32,
-                                                     dirichlet_alpha: f64,
-                                                     c_puct: f32,
-                                                     debug: bool,
-                                                     num_game_thread: i32))));
-    try!(m.add(py, "play_with", py_fn!(py, play_with(object: PyObject,
-                                                     num_simulation: i32,
-                                                     epsilon: f32,
-                                                     dirichlet_alpha: f64,
-                                                     c_puct: f32))));
+    try!(m.add(
+        py,
+        "__doc__",
+        "This module is implemented in Rust, for Simulating Connect6"
+    ));
+    try!(m.add(
+        py,
+        "self_play",
+        py_fn!(
+            py,
+            self_play(
+                object: PyObject,
+                num_simulation: i32,
+                epsilon: f32,
+                dirichlet_alpha: f64,
+                c_puct: f32,
+                debug: bool,
+                num_game_thread: i32
+            )
+        )
+    ));
+    try!(m.add(
+        py,
+        "play_with",
+        py_fn!(
+            py,
+            play_with(
+                object: PyObject,
+                num_simulation: i32,
+                epsilon: f32,
+                dirichlet_alpha: f64,
+                c_puct: f32
+            )
+        )
+    ));
     Ok(())
 });
 
@@ -83,14 +105,16 @@ py_module_initializer!(libconnect6, initlibconnect6, PyInit_connect6, |py, m| {
 ///
 /// If PyObject isn't callable object, pybind::py_policy::AlphaZero::get_from will panic
 ///
-fn self_play(py: Python,
-             object: PyObject,
-             num_simulation: i32,
-             epsilon: f32,
-             dirichlet_alpha: f64,
-             c_puct: f32,
-             debug: bool,
-             num_game_thread: i32) -> PyResult<PyTuple> {
+fn self_play(
+    py: Python,
+    object: PyObject,
+    num_simulation: i32,
+    epsilon: f32,
+    dirichlet_alpha: f64,
+    c_puct: f32,
+    debug: bool,
+    num_game_thread: i32,
+) -> PyResult<PyTuple> {
     let param = policy::HyperParameter {
         num_simulation,
         epsilon,
@@ -99,8 +123,11 @@ fn self_play(py: Python,
     };
     if num_game_thread == 1 {
         let mut policy = policy::AlphaZero::with_param(object, param);
-        let result =
-            if debug { agent::Agent::debug(&mut policy).play() } else { agent::Agent::new(&mut policy).play() };
+        let result = if debug {
+            agent::Agent::debug(&mut policy).play()
+        } else {
+            agent::Agent::new(&mut policy).play()
+        };
         Ok(result.unwrap().to_py_object(py))
     } else {
         let result = py.allow_threads(move || {
@@ -112,11 +139,15 @@ fn self_play(py: Python,
                 };
                 policy::AlphaZero::with_param(object, param)
             };
-            let async_agent =
-                if debug { agent::AsyncAgent::debug(policy_gen) } else { agent::AsyncAgent::new(policy_gen) };
+            let async_agent = if debug {
+                agent::AsyncAgent::debug(policy_gen)
+            } else {
+                agent::AsyncAgent::new(policy_gen)
+            };
             async_agent.run(num_game_thread)
         });
-        let py_result = result.iter()
+        let py_result = result
+            .iter()
             .map(|x| x.to_py_object(py).into_object())
             .collect::<Vec<_>>();
         Ok(PyTuple::new(py, py_result.as_slice()))
@@ -139,12 +170,14 @@ fn self_play(py: Python,
 ///
 /// If PyObject isn't callable object, pybind::py_policy::AlphaZero::get_from will panic
 ///
-fn play_with(py: Python,
-             object: PyObject,
-             num_simulation: i32,
-             epsilon: f32,
-             dirichlet_alpha: f64,
-             c_puct: f32) -> PyResult<PyTuple> {
+fn play_with(
+    py: Python,
+    object: PyObject,
+    num_simulation: i32,
+    epsilon: f32,
+    dirichlet_alpha: f64,
+    c_puct: f32,
+) -> PyResult<PyTuple> {
     let param = policy::HyperParameter {
         num_simulation,
         epsilon,
