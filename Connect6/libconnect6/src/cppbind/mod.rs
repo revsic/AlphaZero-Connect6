ecca5b4c67e0c10754ed3724b61ae46310aeecf1
revsic <revsic99@gmail.com>
2018-09-18 21:02:43 +0900

    Update CppBind - Add impl of Evaluator

--- Connect6/libconnect6/src/cppbind/mod.rs
+++ Connect6/libconnect6/src/cppbind/mod.rs
@@ -1,3 +1,15 @@
+use game::Player;
+use policy::Evaluator;
+use {Board, BOARD_SIZE};
 
 struct CppEval {}
 
+impl Evaluator for CppEval {
+    fn eval(
+        &self,
+        turn: Player,
+        board: &Vec<Board>,
+    ) -> Option<(Vec<f32>, Vec<[[f32; BOARD_SIZE]; BOARD_SIZE]>)> {
+        None
+    }
+}
