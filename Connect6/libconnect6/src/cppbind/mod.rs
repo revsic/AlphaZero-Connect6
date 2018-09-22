c4e727c589a70b58e6fe6254d559a12e5070230f
revsic <revsic99@gmail.com>
2018-09-22 19:31:10 +0900

    Update Cppbind - Implement CppEval

--- Connect6/libconnect6/src/cppbind/mod.rs
+++ Connect6/libconnect6/src/cppbind/mod.rs
@@ -3,7 +3,7 @@ use policy::Evaluator;
 use {Board, BOARD_CAPACITY, BOARD_SIZE};
 
 type CINT = ::std::os::raw::c_int;
-type Callback = extern "C" fn(player: CINT, *mut CINT) -> Result;
+type Callback = extern "C" fn(CINT, *const [[CINT; BOARD_SIZE]; BOARD_SIZE], CINT) -> Result;
 
 #[repr(C)]
 struct Result {
@@ -13,6 +13,16 @@ struct Result {
 
 struct CppEval {
     callback: Callback,
+}
+
+fn convert_to_cint(board: &Board) -> [[CINT; BOARD_SIZE]; BOARD_SIZE] {
+    let mut converted = [[0; BOARD_SIZE]; BOARD_SIZE];
+    for i in 0..BOARD_SIZE {
+        for j in 0..BOARD_SIZE {
+            converted[i][j] = board[i][j] as CINT;
+        }
+    }
+    converted
 }
 
 impl CppEval {
@@ -25,23 +35,30 @@ impl CppEval {
         turn: Player,
         board: &Vec<Board>,
     ) -> Option<(Vec<f32>, Vec<[[f32; BOARD_SIZE]; BOARD_SIZE]>)> {
+        let len = board.len();
         let player = turn as CINT;
-        let mut flatten: Vec<[i32; BOARD_CAPACITY]> = Vec::with_capacity(board.len());
+        let converted = board.iter().map(convert_to_cint).collect::<Vec<_>>();
 
-        for board in board.iter() {
-            let mut iter = 0;
-            let mut flat = [0; BOARD_CAPACITY];
-            for row in board {
-                for elem in row {
-                    flat[iter] = elem as CINT;
+        let result = (self.callback)(player, converted.as_ptr(), len as CINT);
+
+        let value = unsafe { Vec::from_raw_parts(result.value, len, len) };
+        let policy_raw = unsafe { ::std::slice::from_raw_parts(result.policy, len * 4) };
+
+        let mut iter = 0;
+        let mut policy = Vec::with_capacity(len);
+
+        for _ in 0..len {
+            let mut board = [[0.; BOARD_SIZE]; BOARD_SIZE];
+            for i in 0..BOARD_SIZE {
+                for j in 0..BOARD_SIZE {
+                    board[i][j] = policy_raw[iter];
                     iter += 1;
                 }
             }
-
-            flatten.push(flat)
+            policy.push(board);
         }
 
-        let result = (self.callback)(player, flatten.as_mut_ptr());
+        Some((value, policy))
     }
 }
 
