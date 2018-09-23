71175ea02dbf208eb2ed7066cd489a19ed63e327
revsic <revsic99@gmail.com>
2018-09-23 17:09:40 +0900

    Update CppBind Tests - Impl ConvertToCINT test

--- Connect6/libconnect6/src/cppbind/tests.rs
+++ Connect6/libconnect6/src/cppbind/tests.rs
@@ -2,5 +2,17 @@ use super::*;
 
 #[test]
 fn test_convert_to_cint() {
-    assert!(true);
+    let mut board = [[Player::None; BOARD_SIZE]; BOARD_SIZE];
+
+    board[0][0] = Player::Black;
+    board[0][BOARD_SIZE - 1] = Player::White;
+    board[BOARD_SIZE - 1][BOARD_SIZE - 1] = Player::Black;
+    board[BOARD_SIZE - 1][0] = Player::White;
+
+    let result = convert_to_cint(&board);
+
+    assert_eq!(result[0][0], -1);
+    assert_eq!(result[0][BOARD_SIZE - 1], 1);
+    assert_eq!(result[BOARD_SIZE - 1][BOARD_SIZE - 1], -1);
+    assert_eq!(result[BOARD_SIZE - 1][0], 1);
 }
