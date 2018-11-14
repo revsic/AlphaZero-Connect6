573f32a8c5f5928d85427009de4320b5b28ebdd3
revsic <revsic99@gmail.com>
2018-11-14 23:48:59 +0900

    Update Sample Cpp - Add Connect6.hpp

---
+++ Connect6/cppbind/connect6.hpp
@@ -0,0 +1,205 @@
+#ifndef CONNECT6_H
+#define CONNECT6_H
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace Connect6_RustFFI {
+    constexpr size_t BOARD_SIZE = 15;
+    constexpr size_t BOARD_CAPACITY = BOARD_SIZE * BOARD_SIZE;
+
+    using Callback = void(*)(int player, float* values, float* policies, int len);
+
+    template <typename T>
+    using AllocatorType = T*(*)(int size);
+
+    template <typename T>
+    T* allocator(int size) {
+        return new T[size];
+    }
+
+    extern "C" {
+        struct Path {
+            int turn;
+            int board[BOARD_SIZE][BOARD_SIZE];
+            int row;
+            int col;
+        };
+
+        struct RunResult {
+            int winner;
+            Path* paths;
+            int len;
+        };
+
+        struct Vec {
+            RunResult* vec;
+            int len;
+        };
+
+        Vec cpp_self_play(Callback callback,
+                          AllocatorType<Path> alloc_path,
+                          AllocatorType<RunResult> alloc_result,
+                          int num_simulation,
+                          float epsilon,
+                          double dirichlet_alpha,
+                          float c_puct,
+                          bool debug,
+                          int num_game_thread);
+    }
+}
+
+namespace Connect6 {
+    using Connect6_RustFFI::BOARD_SIZE;
+    using Connect6_RustFFI::BOARD_CAPACITY;
+
+    using Connect6_RustFFI::Callback;
+
+    enum class Player : int {
+        Black = -1,
+        None = 0,
+        White = 1,
+    };
+
+    std::string to_string(Player player) {
+        switch (player) {
+        case Player::Black:
+            return { "Black" };
+        case Player::White:
+            return { "White" };
+        case Player::None:
+            return { "None" };
+        }
+    }
+
+    class Path {
+    public:
+        Path() : turn(Player::None), position({ 0, 0 }) {
+            // Do Nothing
+        }
+
+        Path(Player turn, const std::tuple<size_t, size_t>& position, int board_[BOARD_SIZE][BOARD_SIZE]) :
+            turn(turn), position(position)
+        {
+            std::memcpy(board, board_, BOARD_CAPACITY);
+        }
+
+        Path(const Connect6_RustFFI::Path& path) :
+            turn(static_cast<Player>(path.turn)), position({ path.row, path.col })
+        {
+            std::memcpy(board, path.board, BOARD_CAPACITY);
+        }
+
+        Player GetTurn() const {
+            return turn;
+        }
+
+        const std::tuple<size_t, size_t>& GetPos() const {
+            return position;
+        };
+
+        int* operator[](size_t idx) {
+            return board[idx];
+        }
+
+        const int* operator[](size_t idx) const {
+            return board[idx];
+        }
+
+    private:
+        Player turn;
+        std::tuple<size_t, size_t> position;
+        int board[BOARD_SIZE][BOARD_SIZE] = { 0, };
+    };
+
+    class GameResult {
+    public:
+        GameResult() : winner(Player::None), size(0), paths(nullptr) {
+            // Do Nothing
+        }
+
+        GameResult(Player winner, size_t size, std::unique_ptr<Path[]>&& paths) :
+            winner(winner), size(size), paths(std::move(paths))
+        {
+            // Do Nothing
+        }
+
+        GameResult(const Connect6_RustFFI::RunResult& run_result) :
+            winner(static_cast<Player>(run_result.winner)),
+            size(run_result.len),
+            paths(std::make_unique<Path[]>(size))
+        {
+            for (size_t i = 0; i < size; ++i) {
+                paths[i] = Path(run_result.paths[i]);
+            }
+        }
+
+        Player GetWinner() const {
+            return winner;
+        }
+
+        const Path& operator[](size_t idx) const {
+            return paths[idx];
+        }
+
+        size_t GetSize() const {
+            return size;
+        }
+
+        Path* begin() {
+            return paths.get();
+        }
+
+        const Path* begin() const {
+            return paths.get();
+        }
+
+        Path* end() {
+            return paths.get() + size;
+        }
+
+        const Path* end() const {
+            return paths.get() + size;
+        }
+
+    private:
+        Player winner;
+
+        size_t size;
+        std::unique_ptr<Path[]> paths;
+    };
+
+    std::vector<GameResult> self_play(Callback callback,
+                                      int num_simulation,
+                                      float epsilon,
+                                      double dirichlet_alpha,
+                                      float c_puct,
+                                      bool debug,
+                                      int num_game_thread)
+    {
+        namespace FFI = Connect6_RustFFI;
+        FFI::Vec result = FFI::cpp_self_play(
+                callback,
+                &FFI::allocator<FFI::Path>,
+                &FFI::allocator<FFI::RunResult>,
+                num_simulation,
+                epsilon,
+                dirichlet_alpha,
+                c_puct,
+                debug,
+                num_game_thread);
+
+        std::vector<GameResult> game_result;
+        game_result.reserve(result.len);
+
+        for (size_t i = 0; i < result.len; ++i) {
+            game_result.emplace_back(result.vec[i]);
+        }
+
+        return game_result;
+    }
+}
+
+
+#endif
--- Connect6/sample/cpp/main.cpp
+++ Connect6/sample/cpp/main.cpp
@@ -1,48 +1,7 @@
+#include "connect6.hpp"
+
 #include <iostream>
 #include <random>
-
-constexpr size_t BOARD_SIZE = 15;
-constexpr size_t BOARD_CAPACITY = BOARD_SIZE * BOARD_SIZE;
-
-using Callback = void(*)(int player, float* values, float* policies, int len);
-
-template <typename T>
-T* allocator(int size) {
-    return new T[size];
-}
-
-template <typename T>
-using AllocatorType = T*(*)(int size);
-
-extern "C" {
-    struct Path {
-        int turn;
-        int board[BOARD_SIZE][BOARD_SIZE];
-        int row;
-        int col;
-    };
-
-    struct RunResult {
-        int winner;
-        Path* paths;
-        int len;
-    };
-
-    struct Vec {
-        RunResult* vec;
-        int len;
-    };
-
-    Vec cpp_self_play(Callback callback,
-                      AllocatorType<Path> alloc_path,
-                      AllocatorType<RunResult> alloc_result,
-                      int num_simulation,
-                      float epsilon,
-                      double dirichlet_alpha,
-                      float c_puct,
-                      bool debug,
-                      int num_game_thread);
-}
 
 void callback(int player, float* values, float* policies, int len) {
     std::random_device rd;
@@ -54,6 +13,9 @@ void callback(int player, float* values, float* policies, int len) {
         values[i] = dist(gen);
     }
 
+    using Connect6::BOARD_SIZE;
+    using Connect6::BOARD_CAPACITY;
+
     for (size_t i = 0; i < len; ++i) {
         for (size_t j = 0; j < BOARD_SIZE; ++j) {
             for (size_t k = 0; k < BOARD_SIZE; ++k) {
@@ -64,19 +26,8 @@ void callback(int player, float* values, float* policies, int len) {
 }
 
 int main() {
-    Vec result_vec = cpp_self_play(
-            callback,
-            &allocator<Path>,
-            &allocator<RunResult>,
-            2, 0.25, 0.03, 1, true, 1);
+    auto result = Connect6::self_play(callback, 2, 0.25, 0.03, 1, true, 1);
+    std::cout << "Winner : " << to_string(result[0].GetWinner()) << std::endl;
 
-    RunResult result = result_vec.vec[0];
-    std::cout << "Winner : " << (result.winner < 0 ? "Black" : "White") << std::endl;
-
-    for (size_t i = 0; i < result_vec.len; ++i) {
-        delete[] result_vec.vec[i].paths;
-    }
-
-    delete[] result_vec.vec;
     return 0;
 }
