8394ce7c9901fb59caa92e6699660a37801c5c3f
revsic <revsic99@gmail.com>
2018-11-15 21:25:05 +0900

    Update LibConnect6 - Remove cppbind

--- Connect6/libconnect6/src/cppbind/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Rust bindings for C++ interface and impl of AlphaZero policy evaluator
-//!
-//! It provides `extern "C"` based bindings for some utilities to implement policy `AlphaZero` eval.
-//!
-pub use self::cppeval::*;
-pub use self::rawobj::*;
-
-mod cppeval;
-mod rawobj;
