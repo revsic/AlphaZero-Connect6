3924daea3043e541c62f043871c7f0c1a5c247c8
revsic <revsic99@gmail.com>
2018-11-15 21:26:23 +0900

    Update libconnect6 macro_def - Remove pybind relative macro

--- Connect6/libconnect6/src/macro_def/mod.rs
+++ Connect6/libconnect6/src/macro_def/mod.rs
@@ -1,53 +1,10 @@
-pub use self::support::{create_pypolicy, PyPolicy};
-
-use BOARD_CAPACITY;
-
-use cpython::*; // avoid unused import warning of `PythonObject`, `ToPyObject`
-use rand;
-
-mod support;
-
-/// Panic with given error string if given Result is Err
-///
-/// # Examples
-/// ```rust
-/// let pyseq = must!(obj.cast_into::<PySequence>(py), "couldn't cast obj into PySequence");
-/// ```
-#[macro_export]
-macro_rules! must {
-    ($e:expr, $err:expr) => {
-        match $e {
-            Ok(obj) => obj,
-            Err(e) => panic!("{} : {:?}", $err, e),
-        }
-    };
-}
-
-/// Create random python policy for testing policy AlphaZero
-///
-/// # Examples
-/// ```rust
-/// let py_policy = py_policy!();
-/// let mut policy = AlphaZero::new(py_policy);
-/// ```
-#[macro_export]
-macro_rules! py_policy {
-    () => {{
-        use cpython::{Python, PythonObject};
-
-        let gil = Python::acquire_gil();
-        let py = gil.python();
-        $crate::macro_def::create_pypolicy(py)
-            .unwrap()
-            .into_object()
-    }};
-}
-
 /// Create IoPolicy with stdio
 ///
 /// # Examples
-/// ```rust
-/// io_policy_stdio(policy);
+/// ```ignore
+/// # #[macro_use] extern crate connect6;
+/// # use connect6::agent::Agent;
+/// io_policy_stdio!(policy);
 /// let result = Agent::new(&mut policy).play();
 /// assert!(result.is_ok());
 /// ```
