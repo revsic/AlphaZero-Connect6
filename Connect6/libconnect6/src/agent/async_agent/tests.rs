ea17b96aef66495dc9cdee1f2f737569e82c9ba9
revsic <revsic99@gmail.com>
2018-11-15 21:34:43 +0900

    Update LibConnect6 AsyngAgent - Add test_alphazero_run

--- Connect6/libconnect6/src/agent/async_agent/tests.rs
+++ Connect6/libconnect6/src/agent/async_agent/tests.rs
@@ -1,12 +1,34 @@
 use super::*;
 use game::Player;
-use policy::RandomPolicy;
+use policy::{AlphaZero, HyperParameter, RandomEvaluator, RandomPolicy};
 
 use std::time::Instant;
 
 #[test]
 fn test_run() {
     let policy_gen = || RandomPolicy::new();
+    let async_agent = AsyncAgent::debug(policy_gen);
+
+    let now = Instant::now();
+    let result = async_agent.run(4);
+    let elapsed = now.elapsed();
+    println!("{}.{}s elapsed", elapsed.as_secs(), elapsed.subsec_millis());
+
+    assert_eq!(result.len(), 4);
+    for run_result in result {
+        if let Some(last) = run_result.path.last() {
+            if run_result.winner != Player::None {
+                assert_eq!(last.turn, run_result.winner);
+            }
+        }
+    }
+}
+
+#[test]
+fn test_alphazero_run() {
+    let param = HyperParameter::light_weight();
+
+    let policy_gen = || AlphaZero::with_param(Box::new(RandomEvaluator {}), param);
     let async_agent = AsyncAgent::debug(policy_gen);
 
     let now = Instant::now();
