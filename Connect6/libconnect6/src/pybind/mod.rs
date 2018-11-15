103f89553a5cb3f6767d5efb65a6c1635db15d18
revsic <revsic99@gmail.com>
2018-11-15 21:25:43 +0900

    Update LibConnect6 - Remove pybind

--- Connect6/libconnect6/src/pybind/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Rust bindings for python interface and impl of AlphaZero policy evaluator
-//!
-//! It provides `rust-cpython` based bindings for some utilities to implement policy `AlphaZero`.
-//!
-pub use self::pybind_impl::*;
-pub use self::pyeval::*;
-
-mod pybind_impl;
-mod pyeval;
