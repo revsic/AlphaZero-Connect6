62eb60fe1886cecb6e000afd9e5f842ebb1e33a6
revsic <revsic99@gmail.com>
2018-11-17 01:15:13 +0900

    Update Sample - Modify it to show how alphazero policy is run

--- Connect6/sample/rust/src/main.rs
+++ Connect6/sample/rust/src/main.rs
@@ -1,24 +1,15 @@
 //! Rust sample usage of connect6.
 extern crate connect6;
 
-use connect6::{agent, game::Player, policy};
-use std::time::Instant;
+use connect6::{agent, policy};
 
 fn main() {
-    let rand_policy = || policy::RandomPolicy::new();
-    let async_agent = agent::AsyncAgent::debug(rand_policy);
+    let param = policy::HyperParameter::light_weight();
+    let eval = Box::new(policy::RandomEvaluator {});
+    let mut policy = policy::AlphaZero::with_param(eval, param);
 
-    let now = Instant::now();
-    let result = async_agent.run(4);
-    let elapsed = now.elapsed();
-    println!("{}.{}s elapsed", elapsed.as_secs(), elapsed.subsec_millis());
+    let result = agent::Agent::debug(&mut policy).play();
 
-    assert_eq!(result.len(), 4);
-    for run_result in result {
-        if let Some(last) = run_result.path.last() {
-            if run_result.winner != Player::None {
-                assert_eq!(last.turn, run_result.winner);
-            }
-        }
-    }
+    assert!(result.is_ok());
+    println!("{:?} is win", result.unwrap().winner);
 }
