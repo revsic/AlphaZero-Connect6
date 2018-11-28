b11a6f9889e03e09600c434aacbd51929ef00524
revsic <revsic99@gmail.com>
2018-11-28 16:50:15 +0900

    Update LibCppConnect6 - Add raw_play_result test

--- Connect6/cppbind/libcppconnect6/src/cppbind/ffi_test/mod.rs
+++ Connect6/cppbind/libcppconnect6/src/cppbind/ffi_test/mod.rs
@@ -46,15 +46,64 @@ pub extern "C" fn test_echo_raw_path(
     }
 }
 
-// #[no_mangle]
-// pub extern "C" fn test_with_raw_play_result() -> RawPlayResult {
+#[no_mangle]
+pub extern "C" fn test_with_raw_play_result(allocator: AllocatorType<RawPath>) -> RawPlayResult {
+    let mut vec = Vec::new();
+    let mut player = Player::Black;
 
-// }
+    for i in 0..10 {
+        let mut board = [[Player::None; BOARD_SIZE]; BOARD_SIZE];
+        for j in 0..i+1 {
+            board[j][j] = Player::from(((i + j) % 3) as i32 - 1);
+        }
+        vec.push(agent::Path {
+            turn: player,
+            board,
+            pos: (i, i + 1),
+        });
 
-// #[no_mangle]
-// pub extern "C" fn test_echo_raw_play_result() -> RawPlayResult {
+        player.mut_switch();
+    }
 
-// }
+    let result = agent::PlayResult {
+        winner: Player::Black,
+        path: vec,
+    };
+
+    let alloc = Allocator::new(allocator);
+    RawPlayResult::with_result(&result, &alloc)
+}
+
+#[no_mangle]
+pub extern "C" fn test_echo_raw_play_result(
+    winner: CInt,
+    path: *mut RawPath,
+    len: CInt,
+    allocator: AllocatorType<RawPath>,
+) -> RawPlayResult {
+    let path_s = unsafe { ::std::slice::from_raw_parts(path, len as usize) };
+
+    let mut vec = Vec::new();
+    for i in 0..len as usize {
+        let mut board = [[Player::None; BOARD_SIZE]; BOARD_SIZE];
+        for j in 0..BOARD_SIZE {
+            for k in 0..BOARD_SIZE {
+                board[j][k] = Player::from(path_s[i].board[j][k]);
+            }
+        }
+
+        vec.push(agent::Path {
+            turn: Player::from(path_s[i].turn),
+            board,
+            pos: (path_s[i].row as usize, path_s[i].col as usize),
+        });
+    }
+
+    let result = agent::PlayResult { winner: Player::from(winner), path: vec };
+
+    let alloc = Allocator::new(allocator);
+    RawPlayResult::with_result(&result, &alloc)
+}
 
 #[no_mangle]
 pub extern "C" fn test_with_raw_vec(allocator: AllocatorType<CInt>) -> RawVec<CInt> {
