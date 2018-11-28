1340bd906c879299ad5f726e805c749e52dd1f1f
revsic <revsic99@gmail.com>
2018-11-28 14:06:42 +0900

    Update CppBind - Add two more raw_path tests

--- Connect6/cppbind/libcppconnect6/src/cppbind/ffi_test/mod.rs
+++ Connect6/cppbind/libcppconnect6/src/cppbind/ffi_test/mod.rs
@@ -1,5 +1,28 @@
-use connect6::{game::Player, BOARD_CAPACITY, BOARD_SIZE};
+use connect6::{agent, game::Player, BOARD_CAPACITY, BOARD_SIZE};
 use cppbind::{CInt, RawPath};
+
+#[no_mangle]
+pub extern "C" fn test_new_raw_path() -> RawPath {
+    RawPath::new()
+}
+
+#[no_mangle]
+pub extern "C" fn test_with_raw_path() -> RawPath {
+    let mut board = [[Player::None; BOARD_SIZE]; BOARD_SIZE];
+    for i in 0..BOARD_SIZE {
+        for j in 0..BOARD_SIZE {
+            let id = ((i * BOARD_SIZE + j) % 3) as i32 - 1;
+            board[i][j] = Player::from(id);
+        }
+    }
+
+    let path = agent::Path {
+        turn: Player::White,
+        board,
+        pos: (0, BOARD_SIZE % 5 + 1),
+    };
+    RawPath::with_path(&path)
+}
 
 #[no_mangle]
 pub extern "C" fn test_echo_raw_path(
