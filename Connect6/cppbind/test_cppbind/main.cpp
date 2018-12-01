4b4325fe8510525531bb221b3159e03965233beb
revsic <revsic99@gmail.com>
2018-12-01 21:51:38 +0900

    Update Test CppBind - Remove Catch2 and Include External

--- Connect6/cppbind/test_cppbind/main.cpp
+++ Connect6/cppbind/test_cppbind/main.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN
 
 #include "../connect6.hpp"
-#include "catch.hpp"
+#include "catch2/catch.hpp"
 #include <random>
 
 void callback(int player, float* values, float* policies, int len_) {
