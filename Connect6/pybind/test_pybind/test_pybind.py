6d3ec430859afee76c43d2dd51fd930d0f6e03c6
revsic <revsic99@gmail.com>
2018-12-02 02:31:56 +0900

    Update Test PyBind - Add Test for PyEval

--- Connect6/pybind/test_pybind/test_pybind.py
+++ Connect6/pybind/test_pybind/test_pybind.py
@@ -1,6 +1,7 @@
 import pyconnect6
 import numpy as np
 
+
 class RandomPolicy:
     def __init__(self):
         self.board_size = pyconnect6.board_size()
@@ -11,6 +12,7 @@ class RandomPolicy:
         rand_policy = np.random.rand(size, self.board_size * self.board_size)
         return value, rand_policy
 
+
 def test_result_length():
     policy = RandomPolicy()
 
@@ -20,3 +22,28 @@ def test_result_length():
 
     result = pyconnect6.self_play(policy, param)
     assert len(result) == 2
+
+
+def test_echo_pyeval():
+    def gen_player(): return np.random.randint(3) - 1
+    turn = gen_player()
+
+    boards_len = 10
+    board_size = pyconnect6.board_size()
+    boards = [gen_player() for _ in range(boards_len * board_size ** 2)]
+
+    def double_policy(turn, board):
+        size = len(board)
+        value = [i + turn for i in range(size)]
+        board = [[i * 2 for i in x] for x in board]
+        return value, board
+
+    test_echo_pyeval = pyconnect6.pyconnect6.test_echo_pyeval
+    value, policy = test_echo_pyeval(double_policy, turn, boards)
+
+    idx = 0
+    for i in range(boards_len):
+        assert value[i] == i + turn
+        for p in policy[i]:
+            assert p == 2 * boards[idx]
+            idx += 1
