0c9a290601d0b9edd3e42d1ca645924a285cf022
revsic <revsic99@gmail.com>
2018-12-24 03:18:12 +0900

    Update PyWeighted - Fix Buffer path to py_weighted.buffer

--- AlphaZero/py_weighted/train.py
+++ AlphaZero/py_weighted/train.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from datetime import datetime
 
 import pyconnect6
-from buffer import Buffer
+from py_weighted.buffer import Buffer
 from py_weighted.model import WeightedPolicy
 
 flags = tf.app.flags
