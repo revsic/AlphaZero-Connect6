09ea36337316f185fc0e60bb7bd74f112e31dbf2
revsic <revsic99@gmail.com>
2018-12-24 02:42:42 +0900

    Update Cpp Weighted Model - Implement with torch

--- AlphaZero/cpp_weighted/model.hpp
+++ AlphaZero/cpp_weighted/model.hpp
@@ -1,23 +1,44 @@
 #ifndef MODEL_HPP
 #define MODEL_HPP
 
+#include "connect6.hpp"
 #include <torch/torch.h>
 
-class Model : torch::nn::Module {
-public:
-    Model() {
-
+struct WeightedPolicy : torch::nn::Module {
+    WeightedPolicy() : 
+        policy_fc(Connect6::BOARD_CAPACITY + 1, Connect6::BOARD_CAPACITY),
+        value_fc(Connect6::BOARD_CAPACITY + 1, 1)
+    {
+        register_module("policy_fc", policy_fc);
+        register_module("value_fc", value_fc);
     }
 
-    torch::Tensor inference(torch::Tensor player, torch::Tensor board) {
+    auto forward(torch::Tensor player, torch::Tensor board) {
+        player = player.view({-1, 1});
+        torch::Tensor repr = torch::cat({ player, board }, /*dim=*/1);
 
+        torch::Tensor policy = policy_fc->forward(repr);
+        policy = torch::softmax(policy, /*dim=*/1);
+
+        torch::Tensor value = value_fc->forward(repr);
+        value = torch::tanh(value);
+
+        return std::make_tuple(policy, value.view({ -1, }));
     }
 
-    void train(torch::Tensor winner, torch::Tensor player, torch::Tensor board, torch::Tensor policy) {
-
+    torch::Tensor loss(torch::Tensor winner, 
+                       torch::Tensor player, 
+                       torch::Tensor board, 
+                       torch::Tensor policy) 
+    {
+        auto[inf_policy, value] = forward(player, board);
+        torch::Tensor value_loss = torch::mean((winner - value).pow(2));
+        torch::Tensor policy_loss = -torch::mean(torch::sum(policy * torch::log(inf_policy), -1));
+        return value_loss + policy_loss;
     }
 
-private:
+    torch::nn::Linear policy_fc;
+    torch::nn::Linear value_fc;
 };
 
 #endif
