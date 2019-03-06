310d05829d72603509c83505fd7f725de196e569
revsic <revsic99@gmail.com>
2019-03-06 14:44:07 +0900

    cppbind: Add documents

--- Connect6/cppbind/libcppconnect6/src/cppbind/cpp_policy/mod.rs
+++ Connect6/cppbind/libcppconnect6/src/cppbind/cpp_policy/mod.rs
@@ -1,17 +1,24 @@
 use connect6::{game, policy, Board, BOARD_SIZE};
 use cppbind::{board_to_float, CFloat, CInt};
 
+#[cfg(test)]
+mod tests;
+
+/// void(float* boards, int* position_result)
 pub type PolicyCallback = extern "C" fn(*const [[CFloat; BOARD_SIZE]; BOARD_SIZE], *mut [CInt; 2]);
 
+/// C++ FFI policy bindings.
 pub struct CppPolicy {
     callback: PolicyCallback,
 }
 
 impl CppPolicy {
+    /// Create new CppPolicy object.
     pub fn new(callback: PolicyCallback) -> CppPolicy {
         CppPolicy { callback }
     }
 
+    /// Call callback method with given board and return position.
     fn callback(&self, board: &Board) -> Option<(usize, usize)> {
         let mut res: [CInt; 2] = [-1; 2];
         let board_f = board_to_float(board);
--- Connect6/cppbind/libcppconnect6/src/cppbind/mod.rs
+++ Connect6/cppbind/libcppconnect6/src/cppbind/mod.rs
@@ -1,6 +1,6 @@
 //! Rust bindings for C++ interface and impl of AlphaZero policy evaluator
 //!
-//! It provides `extern "C"` based bindings for some utilities to implement policy `AlphaZero` eval.
+//! It provides `extern "C"` based bindings for some utilities to implement policy and `AlphaZero` evaluator.
 //!
 pub use self::cpp_policy::*;
 pub use self::cpp_support::*;
